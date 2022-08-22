5bd2d4b0de2f3450bac97529db3a346c011b10c1
reshul-ai <reshul@braina.live>
2022-08-22 13:14:26 -0700

    Model POC Working

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import {useRef, useEffect} from "react";
+import './App.css';
+import MediapipeFacemesh from './Mediapipefacemesh';
+
+function App() {
+
+  return (
+    <div className="container-fluid">
+        
+       <MediapipeFacemesh />
+       
+    </div>
+  );
+}
+
+export default App;
